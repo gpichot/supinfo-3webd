91f870d52b42ab561d19332229c6cec8cfaae744
Gabriel Pichot <gabriel.pichot@gmail.com>
2023-02-13 14:01:32 +0100

    feat: add textfield

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { TextField } from "@mui/material";
+
 import PokemonCard from "./pokemons/components/PokemonCard";
 import PokemonList from "./pokemons/components/PokemonList";
 import pokemons from "./pokemons.json";
@@ -7,11 +10,25 @@ import "./globals.scss";
 type PokemonList = typeof pokemons;
 
 function App() {
+  const [searchText, setSearchText] = React.useState("");
+  const filteredPokemons = pokemons.filter((pokemon) =>
+    pokemon.name.includes(searchText)
+  );
   return (
     <div>
       <h1>Pokedex</h1>
+      <TextField
+        name="searchText"
+        placeholder="Search pokemons..."
+        value={searchText}
+        style={{
+          width: "100%",
+          marginBottom: 10,
+        }}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
       <PokemonList>
-        {pokemons.map((pokemon) => (
+        {filteredPokemons.map((pokemon) => (
           <PokemonCard key={pokemon.id} pokemon={pokemon} />
         ))}
       </PokemonList>
